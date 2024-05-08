9c7f3c10d735b2ec4b72139ef247da4659718353
Madhusudhan <madhusudhan1863@gmail.com>
2024-05-08 13:29:07 +0530

    fix: wrpa trpc client provider arond layout

--- src/app/layout.client.tsx
+++ src/app/layout.client.tsx
@@ -3,13 +3,16 @@
 import { PropsWithChildren } from "react";
 import { APIProvider } from "@vis.gl/react-google-maps";
 import { Toaster } from "sonner";
+import { TrpcClientProvider } from "@/client";
 
 const HomeLayout = ({ children }: PropsWithChildren) => {
   return (
-    <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!}>
-      {children}
-      <Toaster />
-    </APIProvider>
+    <TrpcClientProvider>
+      <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!}>
+        {children}
+        <Toaster />
+      </APIProvider>
+    </TrpcClientProvider>
   );
 };
 
--- src/client/index.tsx
+++ src/client/index.tsx
@@ -4,6 +4,8 @@ import { inferRouterInputs, inferRouterOutputs } from "@trpc/server";
 import superjson from "superjson";
 
 import { AppRouter } from "@/server";
+import { PropsWithChildren, useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function getBaseUrl() {
   if (typeof window !== "undefined")
@@ -59,3 +61,17 @@ export const trpcStandAloneClient = createTRPCClient<AppRouter>({
 
 export type RouterInput = inferRouterInputs<AppRouter>;
 export type RouterOutput = inferRouterOutputs<AppRouter>;
+
+function ClientProvider(props: PropsWithChildren) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {props.children}
+    </QueryClientProvider>
+  );
+}
+
+export const TrpcClientProvider = trpcClient.withTRPC(
+  ClientProvider
+) as typeof ClientProvider;
