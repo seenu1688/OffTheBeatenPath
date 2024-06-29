a3f435fdc7bb104d135bdb23169b14719dd84259
Madhusudhan <madhusudhan1863@gmail.com>
2024-06-29 18:33:35 +0530

    fix: pass queryclient to the provider

--- src/app/layout.client.tsx
+++ src/app/layout.client.tsx
@@ -1,13 +1,24 @@
 "use client";
 
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useState } from "react";
 import { APIProvider } from "@vis.gl/react-google-maps";
 import { Toaster } from "sonner";
 import { TrpcClientProvider } from "@/client";
+import { QueryClient } from "@tanstack/react-query";
 
 const HomeLayout = ({ children }: PropsWithChildren) => {
+  const [queryClient] = useState(() => {
+    return new QueryClient({
+      defaultOptions: {
+        queries: {
+          refetchOnWindowFocus: false,
+        },
+      },
+    });
+  });
+
   return (
-    <TrpcClientProvider>
+    <TrpcClientProvider queryClient={queryClient}>
       <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!}>
         {children}
         <Toaster />
